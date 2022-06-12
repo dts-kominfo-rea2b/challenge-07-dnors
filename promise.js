46731c43abe7a484ac9dba8bf443d9a4d0e8124d
dnors <deno.nors@gmail.com>
2022-06-12 20:46:19 +0700

    challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput =(emosi) => {
+  return new Promise((resolve, reject) => {
+    let jumlahEmosi = 0;
+    promiseTheaterIXX()
+      .then((hasilSetelahMenonton) => {
+        hasilSetelahMenonton.forEach((item) => {
+          if (item.hasil === emosi) {
+            jumlahEmosi += 1;
+          }
+        });
+      })
+      .then(
+        promiseTheaterVGC().then((hasilSetelahMenonton) => {
+          hasilSetelahMenonton.forEach((item) => {
+            if (item.hasil === emosi) {
+              jumlahEmosi += 1;
+            }
+          });
+          resolve(jumlahEmosi);
+        })
+      )
+      .catch((err) => {
+        reject(err);
+      });
+    
+  })
+}
 
 module.exports = {
   promiseOutput,
